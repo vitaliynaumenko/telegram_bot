f66d48f47dbdca44f78efb4597f6bf68effc094e
vitaliynaumenko <bambiino89@gmail.com>
2023-06-13 19:07:01 +0300

    remove process-nodemon chat gpt-3

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const { Telegraf, inlineKeyboard, Markup, session } = require('telegraf');
-const { Configuration, OpenAIApi} = require('openai')
-require('dotenv').config()
-// const process = require("nodemon");
-const config = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY
-})
-const openAi = new OpenAIApi(config);
-const bot = new Telegraf(process.env.BOT_API_KEY, {polling:true});
+const { Telegraf, inlineKeyboard, Markup } = require('telegraf');
+
+require('dotenv').config();
+
+const botApiKey = process.env.BOT_API
+// console.log(botApiKey);
+// console.log(process.env.BOT_API);
+
+const bot = new Telegraf(botApiKey);
 
 const goodWords = ['чай', 'кава', 'кофе', 'пиво', "пивко","півко", "пивку"]
 const spam = ['шарий', 'чаплыга', 'міха', 'миха', 'чаплига', 'шарій', "шарія", "шария", "зрада"];
@@ -22,6 +22,7 @@ const createKeyBoard = async (ctx)=>{
     }catch (e) {
         console.log(e)
     }
+
 }
 // const whoIsGay =  (arr=[])=>{
 //     if (!arr && !arr.length)return;
@@ -53,7 +54,7 @@ const checkForSpam = (msg)=>{
                     case 'каву':
                         return  msg.replyWithPoll( "Ви можете вибрати місце зустрічі:", ['Терем', "Кофеїн", "Топольок", "Сільмаг", "Наташа", "Амік"], {
                             // open_period: true,
-                            is_anonymous: false,
+                            is_anonymous: true,
                             allows_multiple_answers: true
                         });
 
@@ -63,7 +64,7 @@ const checkForSpam = (msg)=>{
                     case"півко":
                         return  msg.replyWithPoll( "Солоденькі ви хочете сьогодні провести гарно вечір?", ['Так', 'Ні', 'Під каблуком'], {
                             // open_period: 10,
-                            is_anonymous: false,
+                            is_anonymous: true,
                             allows_multiple_answers: true
                         });
 
@@ -82,35 +83,35 @@ const checkForSpam = (msg)=>{
 
 }
 
-const chatGPT = async (ctx)=>{
-    const { message } = ctx.update;
-    const prompt = message.text.replace('/bot_start_chat', '').trim();
-    console.log('prompt', prompt);
-    console.log('msg', ctx.message.text);
-
-    bot.on('message', async ctx =>{
-        try {
-            const response = await openAi.createCompletion({
-                model: "davinci",
-                prompt: prompt,
-                temperature: 0.9,
-                max_tokens: 150,
-                top_p: 1,
-                frequency_penalty: 0.0,
-                presence_penalty: 0.6,
-                stop: ["Human:", " AI:"],
-            })
-            // console.log('response.data.choices', response.data.choices[0]);
-            ctx.reply(response.data.choices[0].text);
-        }catch (e){
-            console.log(e)
-            ctx.reply('Sorry, I was unable to process your message at this time.');
-        }
-    })
-
-
-
-}
+// const chatGPT = async (ctx)=>{
+//     const { message } = ctx.update;
+//     const prompt = message.text.replace('/bot_start_chat', '').trim();
+//     console.log('prompt', prompt);
+//     console.log('msg', ctx.message.text);
+//
+//     bot.on('message', async ctx =>{
+//         try {
+//             const response = await openAi.createCompletion({
+//                 model: "davinci",
+//                 prompt: prompt,
+//                 temperature: 0.9,
+//                 max_tokens: 150,
+//                 top_p: 1,
+//                 frequency_penalty: 0.0,
+//                 presence_penalty: 0.6,
+//                 stop: ["Human:", " AI:"],
+//             })
+//             // console.log('response.data.choices', response.data.choices[0]);
+//             ctx.reply(response.data.choices[0].text);
+//         }catch (e){
+//             console.log(e)
+//             ctx.reply('Sorry, I was unable to process your message at this time.');
+//         }
+//     })
+//
+//
+//
+// }
 
 
 const addAction = (name, src=false, text)=>{
@@ -131,15 +132,10 @@ bot.action(name, async (ctx)=>{
 })
 }
 bot.start(ctx => chatMembers(ctx) );
-bot.command('bot_start_chat', ctx => chatGPT(ctx))
+// bot.command('bot_start_chat', ctx => chatGPT(ctx))
 
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('text', msg => checkForSpam(msg));
-
-// bot.hears(['чай', "кава"], ctx => {
-//     ctx.reply('Мої солоденькі Ви можете випити чай тут:')
-// });
-
 bot.launch();
 // addAction('btn_no', '', 'Сідай на бутилку гімно')
 // addAction('btn_yes', '', `Раз Два Три Підар сьогодні ти ${whoIsGay()}`)
